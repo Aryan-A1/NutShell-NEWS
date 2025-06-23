ae4f9fece8c565e91e792becddf2f9db1f0895a0
Aryan-A1 <aryankumarsingh9370@gmail.com>
2025-06-23 16:50:30 +0530

    update-version

--- src/Components/NewsItem.jsx
+++ src/Components/NewsItem.jsx
@@ -1,18 +1,3 @@
-// const NewsItem = ({title,description,src,url}) => {
-//   return (
-//     <div className="card bg-dark text-light mb-3 inline-block my-3 mx-3 px-2 py-2" style={{maxWidth:"345px"}}>
-//     <img src={src} style ={{height:"200px"}} className="card-img-top" alt="..."/>
-//     <div className="card-body">
-//       <h5 className="card-title">{title}</h5>
-//       <p className="card-text">{description ? description.slice(0.90) : "News not found"}</p>
-//       <a href={url} className="btn btn-primary">Read More</a>
-//     </div>
-//   </div>
-//   )
-// }
-
-// export default NewsItem
-
 const NewsItem = ({ title, description, src, url, author, publishedAt }) => {
   return (
     <div className="card mb-3" style={{ maxWidth: "100%" }}>
